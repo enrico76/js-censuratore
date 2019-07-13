f922057d9770ecab0402a3842cffa1b1cfad7c1d
enrico76 <enricodip@gmail.com>
2019-07-13 19:49:40 +0200

    creating script with two for cicles: one to ask word to censor and one to replace word with xxx

--- main.js
+++ main.js
@@ -3,20 +3,28 @@
 // - Restituisce il testo con xxx al posto delle parole censurate.
 // - Stampa un “badword index” calcolato come il numero di parole censurate
 //  su il numero di parole totali
-var censIn;
+
+// definizione variabili
+var censIn; //variabile d'appoggio
+var paroleIns = [];
+// variabile testo definito da censura
+var sostword = "xxx";
+// variabile testo inserito dall'utente
 var textIn = prompt("inserisci testo da censurare");
+// rendo il contenuto della variabile da stringa ad array
 var textInarray = textIn.split(" ");
+
 console.log(textIn);
 console.log(textInarray);
-var sostword = "xxx";
-// var censIn = [prompt("inserisci parole da censurare nel testo")];
-// console.log(censIn);
-var paroleIns = [];
-for (var i = 0; i < 2; i++) {
+
+
+// chiedo all'utente di inserire 5 parole da censurare
+for (var i = 0; i < 5; i++) {
   censIns = prompt("inserisci parole da censurare nel testo");
   paroleIns.push(censIns);
-
 }
+// percorro per tutta la lunghezza l'array e se trova le parole da censurare
+// le sostituisco con xxx
 for (var j = 0; j < textInarray.length; j++) {
   if (paroleIns.includes(textInarray[j])) {
     textInarray[j] = sostword;
